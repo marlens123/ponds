45420872f842d9ef4e8a2eb0f7783d0fba92793e
marlens123 <marlena@itdex.de>
2023-07-25 10:12:02 +0200

    changes

--- prepare_data.py
+++ prepare_data.py
@@ -6,7 +6,18 @@ import cv2
 from utils.image_transform import crop_center_square, transform_color, resize_image
 from patchify import patchify
 
-############################################
+
+"""
+
+To run this python file, replace 'ir_dir' with path to .nc file of flight 9, and 'mask_dir' with path to annotated training masks.
+
+ir_dir = 'unet_melt_pond_detection/nc_data/flight9/IRdata_ATWAICE_processed_220718_142920.nc'
+mask_dir = 'unet_melt_pond_detection/annotated_data/msks'
+test_mask_dir = 'unet_melt_pond_detection/annotated_data/test_msks'
+
+You also need to adjust the paths where to save results
+
+"""
 
 ir_dir = 'E:/polar/code/data/flight9/IRdata_ATWAICE_processed_220718_142920.nc'
 mask_dir = 'E:/polar/code/data/ir/entire/original_size/msks'
@@ -18,16 +29,20 @@ timestamps = ds.variables['time'][:]
 
 ############## Training Data ###############
 imgs_train = [imgs[2416],imgs[2380],imgs[2424],imgs[2468],imgs[2476],imgs[2708],imgs[3700],imgs[3884]]
+
+############## Test Data ###################
 imgs_test = [imgs[1024], imgs[2452]]
 
 tmp = []
 
+# prepare the training data: crop center square and store and .png file
 for im in imgs_train:
     im = crop_center_square(im)
     tmp.append(im)
 
 imgs_train = tmp
 
+# for experimenting with clipped temperature values (did not make a difference in model performance)
 for idx, img in enumerate(imgs_train):
     plt.imsave('E:/polar/code/data/ir/entire/original_size/ims_raw_normalize/{}.png'.format(idx), img, cmap='gray', 
                vmin=273, vmax=277)
@@ -36,6 +51,7 @@ for idx, img in enumerate(imgs_train):
     plt.imsave('E:/polar/code/data/ir/entire/original_size/ims_raw/{}.png'.format(idx), img, cmap='gray')
 
 
+# prepare masks: transform color values to class values, resize image, crop center square
 masks_train = []
 for f in os.listdir(mask_dir):
     path = os.path.join(mask_dir, f)
@@ -70,6 +86,7 @@ for idx, img in enumerate(imgs_test):
     plt.imsave('E:/polar/code/data/ir/entire/original_size/ims_raw_test/{}.png'.format(idx), img, cmap='gray')
 
 
+# do the same procedure for test images and masks
 masks_test = []
 for f in os.listdir(test_mask_dir):
     path = os.path.join(test_mask_dir, f)
@@ -95,7 +112,7 @@ for im in os.listdir('E:/polar/code/data/ir/entire/original_size/ims_raw_test/')
 imgs_raw_test = np.array(imgs_png)
 masks_raw_test = np.array(masks_test)
 
-# save 480 array
+# save as np array
 np.save('E:/polar/code/data/ir/entire/original_size/ims_raw_np_test/480_im.npy', imgs_raw_test)
 np.save('E:/polar/code/data/ir/entire/original_size/ims_raw_np_test/480_ma.npy', masks_raw_test)
 
